cd98d5c8424782103db9f24f5f04e4acb4944aad
ysk0951 <yskimweb@gmail.com>
2021-01-06 14:02:52 +0900

    keypad kakao internship

---
+++ level1_js_EuclideanAlgorithm.js
@@ -0,0 +1,10 @@
+// /https://programmers.co.kr/learn/courses/30/lessons/12940
+// Euclidean algorithm
+function solution(n, m) {
+
+    //
+    var answer = [];
+    return answer;
+}
+
+console.log(solution(3,12))

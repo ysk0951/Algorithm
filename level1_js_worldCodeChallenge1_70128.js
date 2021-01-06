5a7f1b7440f419bcb27e7154b11c27917f449581
ysk0951 <yskimweb@gmail.com>
2021-01-06 16:58:56 +0900

    level 1 page 2 done

--- level1_js_worldCodeChallenge1_70128.js
+++ level1_js_worldCodeChallenge1_70128.js
@@ -2,6 +2,12 @@
 
 
 function solution(a, b) {
-    var answer = 1234567890;
+    var answer = 0;
+    for(let i =0;i<a.length;i++){
+        answer+=a[i]*b[i];
+    }
+    console.log(answer)
     return answer;
 }
+
+solution([1,2,3,4],[-3,-1,0,2])

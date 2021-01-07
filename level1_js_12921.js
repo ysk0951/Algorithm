8ba816304da17e09172e2d23d8ed3c3e8771ff45
ysk0951 <yskimweb@gmail.com>
2021-01-07 10:47:54 +0900

    'testing'

--- level1_js_12921.js
+++ level1_js_12921.js
@@ -1,20 +1,27 @@
 //https://programmers.co.kr/learn/courses/30/lessons/12921
 
 function solution(n) {
-    let answer = 0;
-    for(let i=2;i<=n;i++){
-        //사이값 i
-        let count = 0;
-        for(let j=1;j<=i;j++){
-            if(i%j==0){
-                count += 1;
-            }
-        }
-        if(count==2){
-            answer++;
-        }
-    }
-    return answer;
+    //1번방법 테스트 케이스 통과햇지만 시간초과
+    // let answer = 0;
+    // let inner = [];
+    // for(let i=2;i<=n;i++){
+    //     //사이값 i
+    //     inner.push(i);
+    // }
+    // for(let i=0;i<inner.length;i++){
+    //     let count = 0;
+    //     for(let j=1;j<=inner[i];j++){
+    //         if(inner[i]%j==0){
+    //             count += 1;
+    //         }
+    //     }
+    //     if(count==2){
+    //         answer += 1;
+    //     }
+    // }
+    // return answer;
+
+    //2번방법
 }
 
-solution(5)
+solution(10)

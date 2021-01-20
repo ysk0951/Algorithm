f000a64a7888dc1de2f4f52f8044802d633e3ba3
ysk0951 <yskimweb@gmail.com>
2021-01-20 15:12:31 +0900

    kakao blind NGame

--- level2/17687.js
+++ level2/17687.js
@@ -1,19 +1,28 @@
 function solution(n, t, m, p) {
     var answer = '';
     var number = []
-    for(var i=0;i<t;i++){
-        var num = ((p-1)+(m*i)).toString(n);
-        console.log(num);
-        if(num.length>1){
-
+    var num = 0;
+    for(var i=0;i<m*t;i++){
+        var numString = num.toString(n);
+        console.log(number);
+        // if(number.length%m==0&&number.length!=0&&answer.length<t){
+        //     answer+=number[p-1];
+        //     number = [];
+        // }
+        if(numString.length==1){
+            number.push(numString[0].toUpperCase())
+            num++;
         }else{
-            number.push((p-1)+(m*i));
+            number.push((numString[0]).toUpperCase());
+            number.push((numString[1]).toUpperCase());
+            num++;
         }
     }
-    console.log(number);
-    console.log(answer);
+    // console.log(answer);
     return answer;
+
 }
 
-solution(2,4,2,1);
-solution(16,16,2,1);
+// solution(2,4,2,1);
+// solution(16,16,2,1);
+solution(12,16,2,2);

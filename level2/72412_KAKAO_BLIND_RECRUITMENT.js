e1454f06fbc0389db914f8be3b57375c5fa55478
ysk0951 <yskimweb@gmail.com>
2021-01-27 12:19:56 +0900

    tree try..

--- level2/72412_KAKAO_BLIND_RECRUITMENT.js
+++ level2/72412_KAKAO_BLIND_RECRUITMENT.js
@@ -1,30 +1,157 @@
 function solution(info, query) {
-    var table = [];
+    //------------------------1번풀이 timeOut----------------------------------------------
+    // var table = [];
+    // var answer = [];
+    // //전처리
+    // for(let i=0;i<info.length;i++){
+    //     var row = info[i].split(' ');
+    //     //Array
+    //     table.push(row);
+    // }
+    // console.log(table);
+    // //search
+    // for(let i=0;i<query.length;i++){
+    //     var search = query[i].split(" ").filter(function(o){
+    //         return o!=="and"
+    //     });
+    //     var overScore = [];
+    //     for(let j of table){
+    //         if(Number(j[4]) >= Number(search[4])){
+    //             if((search[0]==j[0]||search[0]=="-")&&
+    //                 (search[1]==j[1]||search[1]=="-")&&
+    //                 (search[2]==j[2]||search[2]=="-")&&
+    //                 (search[3]==j[3]||search[3]=="-")){
+    //                 overScore.push(j);
+    //             }
+    //         }
+    //     }
+    //     answer.push(overScore.length)
+    // }
+    // return answer;
+
+    // ------------------------2번풀이 ----------------------
     var answer = [];
-    //전처리
-    for(let i=0;i<info.length;i++){
-        var row = info[i].split(' ');
-        //Array
-        table.push(row);
-    }
-    //search
-    for(let i=0;i<query.length;i++){
-        var search = query[i].split(" ").filter(function(o){
-            return o!=="and"
-        });
-        var overScore = [];
-        for(let j of table){
-            if(Number(j[4]) >= Number(search[4])){
-                if((search[0]==j[0]||search[0]=="-")&&
-                    (search[1]==j[1]||search[1]=="-")&&
-                    (search[2]==j[2]||search[2]=="-")&&
-                    (search[3]==j[3]||search[3]=="-")){
-                    overScore.push(j);
+    var table = {
+        "java" : {
+            "backend" : {
+                "senior" : {
+                    "pizza" : [],
+                    "chicken" : []
+                },
+                "junior" : {
+                    "pizza" : [],
+                    "chicken" : []
+                }
+            },
+            "frontend" : {
+                "senior" : {
+                    "pizza" : [],
+                    "chicken" : []
+                },
+                "junior" : {
+                    "pizza" : [],
+                    "chicken" : []
+                }
+            }
+        },
+        "python" : {
+            "backend" : {
+                "senior" : {
+                    "pizza" : [],
+                    "chicken" : []
+                },
+                "junior" : {
+                    "pizza" : [],
+                    "chicken" : []
+                }
+            },
+            "frontend" : {
+                "senior" : {
+                    "pizza" : [],
+                    "chicken" : []
+                },
+                "junior" : {
+                    "pizza" : [],
+                    "chicken" : []
+                }
+            }
+        },
+        "cpp" : {
+            "backend" : {
+                "senior" : {
+                    "pizza" : [],
+                    "chicken" : []
+                },
+                "junior" : {
+                    "pizza" : [],
+                    "chicken" : []
+                }
+            },
+            "frontend" : {
+                "senior" : {
+                    "pizza" : [],
+                    "chicken" : []
+                },
+                "junior" : {
+                    "pizza" : [],
+                    "chicken" : []
                 }
             }
         }
-        answer.push(overScore.length)
+    };
+    //전처리
+    for(let i=0;i<info.length;i++){
+        var row = info[i].split(' ');
+        // console.log(row);
+        table[row[0]][row[1]][row[2]][row[3]].push(row[4]);
     }
-    return answer;
+
+    //search
+    var searchAll = [];
+    for(let i=0;i<query.length;i++) {
+        var search = query[i].split(" ").filter(function(o){
+            return o!=="and"
+        });
+        for(let j=0;j<search.length;j++){
+            if(search[j]=="-"){
+                console.log(search,j)
+                if(j==0){
+                }
+            }
+        }
+        //"-" >> all query
+
+
+
+
+
+
+
+
+
+
+
+
+        // var lan = search[0]
+        // var position = search[1]
+        // var year = search[2]
+        // var food = search[3]
+        // var score = search[4]
+        // if (table[lan] &&
+        //     table[lan][position] &&
+        //     table[lan][position][year] &&
+        //     table[lan][position][year][food]
+        // ) {
+        //     var scores = table[lan][position][year][food];
+        //     var cnt = 0;
+        //     for(let j of scores){
+        //         if(Number(j)>=Number(score)){
+        //             cnt ++;
+        //         };
+        //     }
+        //     answer.push(cnt)
+        // }
+    }
+    // console.log(answer)
 }
 solution(["java backend junior pizza 150", "python frontend senior chicken 210", "python frontend senior chicken 150", "cpp backend senior pizza 260", "java backend junior chicken 80", "python backend senior chicken 50"], ["java and backend and junior and pizza 100","python and frontend and senior and chicken 200","cpp and - and senior and pizza 250","- and backend and senior and - 150","- and - and - and chicken 100","- and - and - and - 150"]);

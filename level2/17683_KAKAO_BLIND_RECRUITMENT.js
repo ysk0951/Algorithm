1d33182b06ef205363179e66c1ace6aa53b5922f
ysk0951 <yskimweb@gmail.com>
2021-01-27 09:38:18 +0900

    rank sort

---
+++ level2/17683_KAKAO_BLIND_RECRUITMENT.js
@@ -0,0 +1,8 @@
+function solution(m, musicinfos) {
+    var answer = '';
+    return answer;
+}
+
+solution("ABCDEFG",["12:00,12:14,HELLO,CDEFGAB", "13:00,13:05,WORLD,ABCDEF"])
+solution("CC#BCC#BCC#BCC#B",["03:00,03:30,FOO,CC#B","04:00,04:08,BAR,CC#BCC#BCC#B"])
+solution("ABC",["12:00,12:14,HELLO,C#DEFGAB","13:00,13:05,WORLD,ABCDEF"])
---
+++ level2/72412_KAKAO_BLIND_RECRUITMENT.js
@@ -0,0 +1,32 @@
+function solution(info, query) {
+    var table = {};
+    var lan = {"cpp":"0","java":"1","python":"2","-":"3"};
+    var work = {"backend":"0","frontend":"1","-":"2"};
+    var years = {"junior":"0","senior":"1","-":"2"};
+    var food = {"chicken":"0","pizza":"1","-":"2"}
+    var searchVal = {};
+    var answer = [];
+    for(let i=0;i<info.length;i++){
+        var row = info[i].split(' ');
+        var rowForTable = [];
+        var lanE = lan[row[0]];
+        var workE = work[row[1]];
+        var yearE = years[row[2]];
+        var foodE = food[row[3]];
+        var score = row[4];
+        var code = lanE+workE+yearE+foodE;
+        rowForTable.push();
+        rowForTable.push(score);
+        table[i] = rowForTable;
+    }
+    for(let i=0;i<query.length;i++){
+        var search = query[i].split("and");
+        var tmp = search[3].split(" ");
+        var score = tmp[2];
+        var code = lan[search[0].trim()]+work[search[1].trim()]+years[search[2].trim()]+food[tmp[1]];
+
+    }
+    console.log(table);
+    return answer;
+}
+solution(["java backend junior pizza 150", "python frontend senior chicken 210", "python frontend senior chicken 150", "cpp backend senior pizza 260", "java backend junior chicken 80", "python backend senior chicken 50"], ["java and backend and junior and pizza 100","python and frontend and senior and chicken 200","cpp and - and senior and pizza 250","- and backend and senior and - 150","- and - and - and chicken 100","- and - and - and - 150"]);

01ef35701be081c31dadfd3d46d7f0a998a50c18
ysk0951 <yskimweb@gmail.com>
2021-04-25 21:03:57 +0900

    stack

---
+++ level2/기능개발.js
@@ -0,0 +1,15 @@
+function solution(progresses, speeds) {
+
+/*
+*   스택/큐
+*   스택은 순서/큐는 진행
+*/
+
+    var queeu = [];
+    var answer = [];
+    console.log(answer);
+    return answer;
+}
+
+
+solution([93,30,55],[1,30,5]); //return [2,1]

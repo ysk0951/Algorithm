b94002a3fe478335d6ca05aff18a2f8eb432eb51
ysk0951 <yskimweb@gmail.com>
2021-04-26 17:47:38 +0900

    done

---
+++ Practice/Study/프린터[Stack+Queue].js
@@ -0,0 +1,26 @@
+function solution(priorities, location) {
+    
+    //Queue
+    let check = false;
+    var rotate = priorities;
+    let tmp = [];
+    for (let i = 0; i < priorities.length; i++) {
+        let priority = priorities[i];
+        let rotate = [];
+        for(let j=i;j<priorities.length;j++){
+            if(priorities[j]>priority){
+            }else{
+            }
+        }
+
+    }
+    var answer = 0;
+    return answer;
+}
+
+solution([2,1,3,2],2);
+
+/**
+ * 순서대로 -> Queue
+ * 
+ */

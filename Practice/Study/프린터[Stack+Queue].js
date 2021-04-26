e9fa4cc4516b7681bd34ae04d1c3f9150eaecc63
ysk0951 <yskimweb@gmail.com>
2021-04-26 18:32:56 +0900

    2021.04.26 문제풀이 끝

--- Practice/Study/프린터[Stack+Queue].js
+++ Practice/Study/프린터[Stack+Queue].js
@@ -2,18 +2,19 @@ function solution(priorities, location) {
     
     //Queue
     let check = false;
-    var rotate = priorities;
     let tmp = [];
+    let rotate = [];
     for (let i = 0; i < priorities.length; i++) {
         let priority = priorities[i];
-        let rotate = [];
         for(let j=i;j<priorities.length;j++){
             if(priorities[j]>priority){
+                rotate.push(priority);
             }else{
+                tmp.push(priority);
             }
         }
-
     }
+    console.log(tmp,rotate);
     var answer = 0;
     return answer;
 }

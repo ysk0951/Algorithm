752ce5e4b945e6b69461aa4a53d3bf24fe5bbf3b
ysk0951 <yskimweb@gmail.com>
2021-05-04 17:31:43 +0900

    thinkin...g

---
+++ Practice/Study/타겟넘버[깊이우선+넓이우선].js
@@ -0,0 +1,59 @@
+/**
+ * 넓이우선..? 깊이 우선..?
+ * 
+ */
+
+// //1번풀이중 막힘 - 시간복잡도 n^3
+// function solution(numbers, target) {
+//     var answer = 0;
+//     var sum = 0 ;
+//     for(let i=0;i<numbers.length;i++){
+//         sum += numbers[i];
+//     }
+//     // 총합에서 target과의 차만큼의 수를 만드는 경우의 수 
+//     // 선택갯수는 상관없음 -> 선택갯수는 원소의 갯수까지 갈수있음
+//     // 목표
+//     var ret = (sum - target)/2;
+//     for(let i=0;i<numbers.length;i++){
+//         //선택갯수 (Count of selected Element )
+//         var COE = i+1;
+//         //각 선택갯수별 조합
+//         for(let j=1;j<=COE;j++){
+//             //J 는 선택갯수
+//             var loop = 1;
+//             while(loop<=j)
+//                 for(let k=0;k<numbers.length;k++){
+                    
+//                 }
+//             j++
+//         }
+//     }
+//     return answer;
+// }
+
+// 2번풀이
+function solution(numbers, target) {
+    var arr = {};
+    for(let i=0;i<numbers.length;i++){
+        if(!arr.objAdd&&i==0){
+            var objAdd = {
+                dept0 : [numbers[i]]
+            }
+            var objSub = {
+                dept0 : [numbers[i]*(-1)]
+            }
+            arr["objAdd"] = objAdd;
+            arr["objSub"] = objSub;
+        }else{
+            var dept = "dept" + (i)
+            var beforeDept = "dept" + (i-1)
+            for(let k=0;k<beforeDept.length;k++){
+
+            }
+            arr['objAdd'][dept] = 
+        }
+    }
+    console.log(arr);
+}
+
+solution([1,1,1,1,1],3);
--- geeksForGeeks/Summary/summary.js
+++ geeksForGeeks/Summary/summary.js
@@ -1,5 +1,16 @@
 /**
+ * 잘게 나눠서 풀면 다풀린다
+ * 
  * [Searching/Sort]
  * Queue + while
- * Array + QuickSort
+ * Array +  V QuickSort(Divide loop)
+ * 
+ * [Greedy/Dynamic]
+ * Greedy - small
+ * Dyamic - big
+ * 
+ * BinarySearchTree : Left Child < Right Child
+ * BinaryTree : Left Child ,Right Child
+ * Hasing :  Key-> Function -> Table
+ * Heap : Parent > Child or Parent < Child
  */

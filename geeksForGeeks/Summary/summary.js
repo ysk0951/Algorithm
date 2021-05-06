9bdc9d1f0cfcd95e63c392c1a1eb744160c31821
ysk0951 <yskimweb@gmail.com>
2021-05-06 13:54:41 +0900

    Info : 'summary.js modified

--- geeksForGeeks/Summary/summary.js
+++ geeksForGeeks/Summary/summary.js
@@ -1,14 +1,12 @@
 /**
  * 잘게 나눠서 풀면 다풀린다
  * 
- * [Searching/Sort]
- * Queue + while
- * Array +  V QuickSort(Divide loop)
+ * *** 데이터 타입별 키워드 ***
+ * 선형(Queue) - Array
+ * 중간중간 데이터가 무차별 삽입 - Tree
  * 
- * [Greedy/Dynamic]
- * Greedy - small
- * Dyamic - big
  * 
+ * *** 익숙치 않은 데이터 타입 ***
  * BinarySearchTree : Left Child < Right Child
  * BinaryTree : Left Child ,Right Child
  * Hasing :  Key-> Function -> Table

95b82bb5313dee51e650513eb5eb23168deb5f4a
yskim-we <abcm556@naver.com>
2021-12-15 00:26:39 +0900

    Sort 1015 ...ing

--- 2022/Sort/1015.py
+++ 2022/Sort/1015.py
@@ -15,6 +15,14 @@
 #그 : 
 
 #Code
+# B[P[0]] =  2 =  A[0] 
+# B[P[1]] =  3 =  A[1] 
+# B[P[2]] =  1 =  A[2] 
+# B[2] =  ?
+# B[3] =  ?
+# B[1] =  ?
+
+#P수열에 대한 내용을 하나씩 다 적어보기
 N = int(input());
 element = list(map(int,input().split(" ")));
 

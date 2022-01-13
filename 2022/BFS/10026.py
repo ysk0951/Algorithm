2bb37b5e40bd8d1701f8f3bf55de04e54eb8103d
김영성 <yskimweb@gmail.com>
2022-01-13 11:11:55 +0900

    formay modify

--- 2022/BFS/10026.py
+++ 2022/BFS/10026.py
@@ -1,8 +1,8 @@
 #탐 : 당장의 결정이 향후의 수를 줄여줌
 #엘 : LIS(bisect) LIS에 대한 아이디어인가
 #이 : start를 end쪽으로 절반씩 줄여나가는가? (정렬이 기본)
-#디 : 2차원 배열에서 4방면을 모두 검색해야하는경우
-#비 : 2차원 배열에서 한루트를 끝까지 검새하는경우
+#디 : 전체루트를 한바퀴 검색하는경우
+#비 : 한루트를 끝까지 검새하는경우
 #다 : 캐싱이 반복적으로 사용되는경우
 #그 : 기타 그래프 이론(사이클관련)
   # 다익스트라
@@ -10,11 +10,28 @@
   # 크루스칼
 
 #Code
+rows = int(input());
+graph = [];
+for i in range(rows):
+  graph.append(list(input()));
+
+visited = [False];
+
+from collections import deque;
+
+def bfs(graph , node , visited):
+  pass;
+
+
 
 #Case
 '''
-A
-B
+5
+RRRBB
+GGBBB
+BBBRR
+BBRRR
+RRRRR
 >>>
-C
+4 3
 '''
